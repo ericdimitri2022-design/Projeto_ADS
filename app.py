3f39e43b71767da18494aa52f3e35af150364359
ericdimitri2022-design <ericdimitri.2022@gmail.com>
2025-10-07 13:40:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 
 app = Flask(__name__)
 
-# Rota principal da página de espera
+# Rota para a página de espera com contador
 @app.route('/espera')
 def pagina_espera():
     # Pega a URL de destino do parâmetro na URL (ex: ?destino=...)
@@ -16,13 +16,12 @@ def pagina_espera():
     # Renderiza o arquivo espera.html e passa a URL de destino para dentro dele
     return render_template('espera.html', destino_url=destino_url)
 
-# Rota para a página inicial
+# Rota para a página inicial (para testes e para dar uma "cara" ao site)
 @app.route('/')
 def index():
-    # Renderiza a página principal a partir de um arquivo HTML
+    # Renderiza a página principal a partir do arquivo index.html
     return render_template('index.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-    
